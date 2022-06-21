61369f798ee4030906be62771f36cc3ae4c73f3e
Gert Halberg <gert.halberg@gmail.com>
2022-06-21 23:41:45 +0200

    Update Configuration.h
    Filament runout activated

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1564,9 +1564,10 @@
  * RAMPS-based boards use SERVO3_PIN for the first runout sensor.
  * For other boards you may need to define FIL_RUNOUT_PIN, FIL_RUNOUT2_PIN, etc.
  */
-//#define FILAMENT_RUNOUT_SENSOR
+#define FILAMENT_RUNOUT_SENSOR
 #if ENABLED(FILAMENT_RUNOUT_SENSOR)
   #define FIL_RUNOUT_PIN E0_DIAG_PIN
+  #define FIL_RUNOUT2_PIN E1_DIAG_PIN
   #define FIL_RUNOUT_ENABLED_DEFAULT true // Enable the sensor on startup. Override with M412 followed by M500.
   #define NUM_RUNOUT_SENSORS   2          // Number of sensors, up to one per extruder. Define a FIL_RUNOUT#_PIN for each.
 
