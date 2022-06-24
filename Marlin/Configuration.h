3308f2cee19e82103d67bd684af0a84eceef8817
Gert Halberg <gert.halberg@gmail.com>
2022-06-24 21:30:00 +0200

    Update Configuration.h
    Make room for fan in y-direction

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1492,7 +1492,7 @@
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS -30
-#define Y_MIN_POS -17
+#define Y_MIN_POS -16
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE + 30
 #define Y_MAX_POS Y_BED_SIZE
